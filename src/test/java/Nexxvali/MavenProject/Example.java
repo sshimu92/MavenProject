fdf59462e581bcf045ac05951e572acba510bb7f
Shimu <shawon.shimu@yahoo.com>
2024-06-06 17:56:22 -0700

    MavenProject

---
+++ src/test/java/Nexxvali/MavenProject/Example.java
@@ -0,0 +1,19 @@
+package Nexxvali.MavenProject;
+
+import org.junit.Test;
+
+public class Example {
+
+	@Test
+	public void addSum() {
+		
+		System.out.println("Sum");
+	}
+	
+	@Test
+	public void lastClass() {
+		
+		System.out.println("Good bye!!!!!!!!!!!!!!!!!!");
+	}
+	
+}
